927c4d3f402d8605975e9f8e02395fb21668b598
Ido Tanne <idovader@gmail.com>
2021-03-29 17:05:09 -0400

    Task: Initial webpack & postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
     context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
+    entry: './main.js',
     output: {
         path: path.resolve(__dirname, 'public')
     },
@@ -14,7 +17,30 @@ module.exports = {
                 test:  /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.(scss|css)/,
+                exclude: /node_modules/,
+                use:[
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            reloadAll: true
+                        }
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ],
             }
         ]
-    }
+    },
+    plugins: [
+        new MinifyPlugin({}, {
+            comments: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
 }
