f870545406df952d7ef96a37ebe0d8eff41449e3
Ido Tanne <idovader@gmail.com>
2021-04-08 14:23:12 -0400

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,42 @@
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    // Set the mode to development or production
+    mode: 'development',
     watch: true,
 
-    mode: 'development',
+    // Control how source maps are generated
     devtool: 'source-map',
     context: path.resolve(__dirname, 'src'),
-    entry: './main.js',
+
+    entry: {
+        index: './main.js',
+    },
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'js/bundle.js'
     },
-    module: {
-        rules: [
-            {
-                test:  /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.(scss|css)/,
-                exclude: /node_modules/,
-                use:[
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            reloadAll: true
-                        }
-                    },
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ],
-            }
-        ]
-    },
     plugins: [
-        new MinifyPlugin({}, {
-            comments: false
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
         new CopyPlugin({
             patterns: [
-                { from: path.resolve(__dirname, 'src'),
+                { from: "./",
                     globOptions: {
                         ignore: [
-                            '**/js/'
+                            '**/js/*',
                         ]
                     }
-                }
-            ],
-            options:{
-                concurrency: 100
-            }
-        })
-    ]
-}
+                }],
+            options: {
+                concurrency: 100,
+            },
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'docs'),
+        open: true,
+        compress: true,
+        port: 3000,
+
+    },
+};
